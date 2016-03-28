c8ad48ab6a0a29b48200a7b7c38f619113bae555
nader-dab <nader.dabour@abv.bg>
2016-03-28 14:51:54 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,358 @@
-# translations-com
+# Telerik.Sitefinity.Translations.TranslationsCom
+When working with the Sitefinity *Translation* module, you can benefit from a number of translation connectors that you use out-of-the-box with minimum setup. You can, however, implement your own translation connector with custom logic to serve your requirements. 
+
+This tutorial provides you with a sample that you use to implement a custom translation connector to work with the **Translations.com** service. You first create and setup the connector and then use the *Translation* API to implement the overall translation process.   
+## Requirements
+Sitefinity 8.1 Enterprise license
+
+## Prerequisites
+
+Your Sitefinity Web site must be in multilingual mode meaning that you have added atleast one additinal language to the current website you are browsing. Otherwise you will not see the translations screen in the administrations tab of your application.
+
+You should either use country specific languages like 'en-US' and not just 'en' or specify a mapping between the country invariant and country specific language in the translations advanced settings screen: <i>Administration >> Settings >> Advanced >> Culture mappings </i> text box.
+
+The Translations.com project that you are using must be configured to translate XLIFF file format.
+The Translations.com project that you are using must be configured for the language translations that you wish to achieve like en-US -> fr-FR; en-us -> de-DE.
+
+Add the *Translation* sample project to your solution. To do this:
+
+1. In Visual Studio, open your Sitefinity web application solution.
+2. In the **SitefinityWebApp**, add a reference to the <strong>Telerik.Sitefinity.Translations.TranslationsCom</strong> assembly.
+
+
+## Create and configure the connector
+
+To configure the *Translation.com* connector in Sitefinity:
+
+1. Navigate to <i>Administration >> Settings >> Advanced >> Translations >> Connectors >> Create new</i>.
+2. In <i>Connector name</i>, enter <strong>Translations.com</strong>.
+3. In <i>Connector title</i>, enter <strong>Translations.com</strong>.
+4. In <i>Connector type</i>, enter <strong>Telerik.Sitefinity.Translations.TranslationsCom.TranslationsComConnector</strong>.
+5. To enable the connector, in the <i>Enabled</i> field, enter <strong>true</strong>.
+6. Save your changes.
+7. Expand the <i>Parameters</i> section of the newly created connector and create the following <i>Keys</i>: 
+   **NOTE:** The following parametersm except for the last one, must be provided by Translations.com.
+ * <strong>url</strong> </br>In <i>Value</i>, enter the URL of the connector
+ * <strong>username</strong> </br>In <i>Value</i>, enter the username
+ * <strong>password</strong> </br>In <i>Value</i>, enter the password
+ * <strong>userAgent</strong> </br>In <i>Value</i>, enter the agent
+ * <strong>project</strong> </br>In <i>Value</i>, enter the name of the project
+ * <strong>fileFormatProfile</strong> </br>In <i>Value</i>, enter the file format that the connector accepts. For example, enter **XLIFF**.
+ * <strong>submissionPrefix</strong> </br>In <i>Value</i>, enter the prefix for the translation submission name that is generated and sent to the connector.
+
+##API Overview: TranslationsComConnector
+The <strong>TranslationsComConnector</strong> class has properties that hold information about the connector. The following table summarizes these API properties.
+
+**NOTE:** In this tutorial, you do not use all of the methods listed below. You can use the table below as reference, as well.
+#### TranslationsCom specific properties
+<table>
+	<thead>
+		<tr>
+			<td><strong>Type</strong></td>
+			<td><strong>Property</strong></td>
+			<td><strong>Description</strong></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><strong><code>ProjectDirectorConfig</code></strong></td>
+			<td><strong>ProjectDirectorConfig</strong></td>
+			<td>
+				<p>
+					Object containing the configurations for the <i>Project Director Connector</i>.
+				</p>
+			</td>
+		</tr>
+		<tr>
+			<td><strong><code>object</code></strong></td>
+			<td><strong>SubmissionNamePrefix</strong></td>
+			<td>
+				<p>
+					The prefix value of the submission names.
+				</p>
+			</td>
+		</tr>
+		<tr>
+			<td><strong><code>string</code></strong></td>
+			<td><strong>TranslationsComProjectShortCode</strong></td>
+			<td>
+				<p>
+					The short code of the <i>translations.com</i> project that this connector uses.
+				</p>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+
+The <strong>TranslationsComConnector</strong> class has several overriden methods from the <strong>TranslationConnectorBase</strong>. The following tables provides more details about each method. 
+
+**Initialization methods**
+<table>
+	<thead>
+		<tr>
+			<td><strong>Type</strong></td>
+			<td><strong>Method</strong></td>
+			<td><strong>Description</strong></td>
+            <td><strong>Required</strong></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><strong><code>void</code></strong></td>
+			<td><strong>InitializeConnector</strong></td>
+			<td>
+				<p>
+					Initializes the connector. The parameter is a <code>NameValueCollection</code> object with TranslationsCom specific configuration parameters.
+				</p>
+			</td>
+            <td>
+				Yes
+            </td>
+		</tr>
+	</tbody>
+</table>
+
+**Send methods**
+Methods below are listed in the order that they are called when the entire translation project is sent to the external translation agency.
+<table>
+	<thead>
+		<tr>
+			<td><strong>Type</strong></td>
+			<td><strong>Method</strong></td>
+			<td><strong>Description</strong></td>
+			<td><strong>Required</strong></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><strong><code>void</code></strong></td>
+			<td><strong>OnInitTranslationJobContext</strong></td>
+			<td>
+				<p>
+Override this method if you need to set initial values to the job context.
+				</p>
+			</td>
+			<td>
+				No
+			</td>
+		</tr>
+		<tr>
+			<td><strong><code>bool</code></strong></td>
+			<td><strong>ProcessStartProjectEvent</strong></td>
+			<td>
+				<p>
+					You use this method for processing the <code>IStartProjectTaskEvent</code> that informs the translation agenecy a new project started. The last parameter of the method stores the external ID for the project that is later used to associate the translations project with the Sitefinity project. Upon the event of succesfully sent message, the method returns **true**.
+				</p>
+			</td>
+			<td>
+				Yes
+            </td>
+		</tr>
+<tr>
+			<td><strong><code>bool</code></strong></td>
+			<td><strong>ProcessCompleteProjectEvent</strong></td>
+			<td>
+				<p>
+					You use this method for processing the <code>ICompleteProjectTaskEvent</code> that informs the translation agency the translation project is complete. pon the event of succesfully sent message, the method returns **true**.
+				</p>
+			</td>
+			<td>
+				Yes
+            </td>
+		</tr>
+		<tr>
+			<td><strong><code>bool</code></strong></td>
+			<td><strong>OnStartProjectError</strong></td>
+			<td>
+				<p>
+				       This method is called if any error occurs when sending the message for starting the project. The following parameters are passed to the method:
+<ul>
+<li>The exception returned from the connector</li>
+<li><code>IStartProjectTaskEvent</code></li>
+<li>The translation job context</li>
+</ul>
+
+If the exception is handled in the method, the method returns **false**. Otherwise, the method returns **true**.
+				</p>
+			</td>
+			<td>
+				No
+            </td>
+		</tr>
+		<tr>
+			<td><strong><code>bool</code></strong></td>
+			<td><strong>ProcessSendTranslationEvent</strong></td>
+			<td>
+				<p>
+					You use this method for processing the translation sent to the external translation service. The following parameters are passed to the method:
+<ul>
+<li><code>ISendTranslationTaskEvent</code></li> containing information about the translation to be sent
+<li><code>ITranslationJobContext</code> job context.</li>
+<li><code>translationId</code> store the external ID for the translation that is later used to associate the translated message with the Sitefinity translation.</li>
+</ul>
+If the method has successfully sent the translation, it returns **true**. 
+					This method checks if the process of sending the translation is successful. 
+				</p>
+			</td>
+			<td>
+				Yes
+            </td>
+		</tr>
+		<tr>
+			<td><strong><code>bool</code></strong></td>
+			<td><strong>OnSendTranslationError</strong></td>
+			<td>
+				<p>
+					This method is called if any error occures when sending the tranlation message. The following parameters are passed to the method:
+<ul>
+<li>The exception that is returned from the connector</li>
+<li>The send translation task event</li>
+<li>The translation job context that is shared between messages sent through the translation connector.</li>
+</ul>
+If the exception is handled in the method, the method returns **false**, otherwise the method returns **true**.
+				</p>
+			</td>
+			<td>
+				No
+            </td>
+		</tr>
+		<tr>
+			<td><strong><code>void</code></strong></td>
+			<td><strong>OnEndSendTranslationJob</strong></td>
+			<td>
+				<p>
+					This method is called after all messages are processed. The parameter of the method is the translation job context.
+				</p>
+			</td>
+			<td>
+				No
+            </td>
+		</tr>
+	</tbody>
+</table>
+
+**Sync methods**
+Methods below are listed in the order, in which they are called during the synchronization of the translations:
+<table>
+	<thead>
+		<tr>
+			<td><strong>Type</strong></td>
+			<td><strong>Method</strong></td>
+			<td><strong>Description</strong></td>
+			<td><strong>Required</strong></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><strong><code>IEnumerable&#60;object&#62;</code></strong></td>
+			<td><strong>GetRawMessages</strong></td>
+			<td>
+				<p>
+					This method gets the raw messages provided by the external translation agency. The messages can be of any kind.
+				</p>
+			</td>
+			<td>
+				Yes
+            </td>
+		</tr>
+		<tr>
+			<td><strong><code>IEnumerable&#60;SyncEventMessage&#62;</code></strong></td>
+			<td><strong>ExtractSyncEventMessages</strong></td>
+			<td>
+				<p>
+This method is called for every raw message returned by the <code>GetRawMessages</code>. For every raw message, one or more translation events can be returned.
+				</p>
+			</td>
+			<td>
+				Yes
+            </td>
+		</tr>
+		<tr>
+			<td><strong><code>bool</code></strong></td>
+			<td><strong>AcknowledgeMessage</strong></td>
+			<td>
+				<p>
+Send a confirmation message to the connector for the succesfull delivery of the translation, so that the status of the translation is updated from completed to delivered. If the delivery is successful, the cofirmation returns **true**.
+				</p>
+			</td>
+			<td>
+				Yes
+            </td>
+		</tr>
+		<tr>
+			<td><strong><code>void</code></strong></td>
+			<td><strong>OnEndSyncTranslationJob</strong></td>
+			<td>
+				<p>
+					This method is called when the synchronization with the connector is finished.
+				</p>
+			</td>
+			<td>
+				Yes
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+**Used types**
+
+<table>
+	<thead>
+		<tr>
+			<td><strong>Type</strong></td>
+			<td><strong>Description</strong></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><strong><code>ISendTranslationTaskEvent</code></strong></td>
+			<td>
+				<p>
+					Contains the information about the translation job, source, and target languages. Holds a reference to the generated XLIFF file.
+				</p>
+			</td>
+		</tr>
+	<tr>
+			<td><strong><code>ITranslationJobContext</code></strong></td>
+			<td>
+				<p>
+					Provides contextual information about the current project. Items can be stored in the context and used during the send process execution.
+				</p>
+			</td>
+		</tr>
+	<tr>
+			<td><strong><code>IStartProjectTaskEvent</code></strong></td>
+			<td>
+				<p>
+					A message that is sent to the translation agency. The message contains meta information such as *StartDate*, *EndDate*, and the name of the project. 
+				</p>
+			</td>
+		</tr>
+	<tr>
+			<td><strong><code>ICompleteProjectTaskEvent</code></strong></td>
+			<td>
+				<p>
+					Contains references to the translation and to the project.
+				</p>
+			</td>
+		</tr>
+	<tr>
+			<td><strong><code>ITranslationSyncContext</code></strong></td>
+			<td>
+				<p>
+					Provides contextual information for the current project. Items can be stored in the context and are available during the sync execution.				
+				</p>
+			</td>
+		</tr>
+	<tr>
+			<td><strong><code>SyncEventMessage</code></strong></td>
+			<td>
+				<p>
+					This message contains the raw message that comes from the connector. The message is used to acknowledge the acceptance of the message from the external translation agency.
+				</p>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
---
+++ Telerik.Sitefinity.Translations.TranslationsCom/TranslationsComConnector.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Xml.Serialization;
+using GlobalLink.Connect;
+using GlobalLink.Connect.Config;
+using GlobalLink.Connect.Model;
+using Telerik.Sitefinity.Translations.Events;
+using Telerik.Sitefinity.Translations.Xliff.Model;
+
+namespace Telerik.Sitefinity.Translations.TranslationsCom
+{
+    /// <summary>
+    /// The Translations.com connector. The main component that connects the Sitefinity CMS with the Translations.com API. 
+    /// It uses the <see cref="GLExchange"/> component that can send and receive messages to and from the Translations.com SOAP API.
+    /// </summary>
+    public class TranslationsComConnector : TranslationConnectorBase
+    {
+        /// <summary>
+        /// Gets or sets the object containing the configurations for the Project Director Connector.
+        /// </summary>
+        /// <value>The project director connector configuration.</value>
+        protected ProjectDirectorConfig ProjectDirectorConfig { get; set; }
+
+        /// <summary>
+        /// Gets or sets the prefix value of the submission names.
+        /// </summary>
+        /// <value>The submission name prefix.</value>
+        protected object SubmissionNamePrefix { get; set; }
+
+        /// <summary>
+        /// Gets or sets the short code (something like id) of the translations.com's project that this connector uses.
+        /// </summary>
+        /// <value>The translations.com project short code.</value>
+        protected string TranslationsComProjectShortCode { get; set; }
+
+        #region Initialization
+        /// <summary>
+        /// Initializes the connector.
+        /// </summary>
+        /// <param name="config">The configuration parameters.</param>
+        protected override void InitializeConnector(NameValueCollection config)
+        {
+            this.ProjectDirectorConfig = this.GetProjectDirectorConfig(config);
+            this.TranslationsComProjectShortCode = config[ConfigKeyConstants.ProjectKey];
+            if (string.IsNullOrEmpty(this.TranslationsComProjectShortCode))
+            {
+                this.ThrowConfigurationMissingError(ConfigKeyConstants.ProjectKey);
+            }
+
+            this.SubmissionNamePrefix = config[ConfigKeyConstants.PrefixKey];
+        }
+        #endregion
+
+        #region Send methods
+        /// <inheritdoc />
+        protected override bool OnSendTranslationError(Exception err, ISendTranslationTaskEvent evnt, ITranslationJobContext context)
+        {
+            return false;
+        }
+
+        /// <inheritdoc />
+        protected override bool ProcessSendTranslationEvent(ISendTranslationTaskEvent evnt, ITranslationJobContext context, out string translationId)
+        {
+            GLExchange projectDirectorClient;
+            if (!context.TryGetItem<GLExchange>(CurrentClientKey, out projectDirectorClient))
+            {
+                var submissionTicket = evnt.ProjectExternalId;
+
+                projectDirectorClient = new GLExchange(this.ProjectDirectorConfig);
+                if (!string.IsNullOrEmpty(submissionTicket))
+                    projectDirectorClient.getSubmission(submissionTicket);
+
+                context.Items[CurrentClientKey] = projectDirectorClient;
+            }
+
+            var translationsComDocument = new Document
+            {
+                fileformat = TranslationsComConnector.FileFormat,
+
+                // Setting the translation id as the name of the document.
+                name = string.Format("{0}.xlf", evnt.TranslationId),
+                sourceLanguage = evnt.ActualSourceLanguage,
+                targetLanguages = new string[] { evnt.TargetLanguage }
+            };
+
+            var xliffFile = evnt.GetXliffFile();
+            this.ProcessXliffFile(xliffFile);
+            var root = new XliffRoot();
+            root.Files.Add(xliffFile);
+
+            var xliffSerializer = new XmlSerializer(typeof(XliffRoot));
+            using (var xliffStream = new MemoryStream())
+            {
+                xliffSerializer.Serialize(xliffStream, root);
+                xliffStream.Seek(0, SeekOrigin.Begin);
+                translationsComDocument.setDataFromMemoryStream(xliffStream);
+                projectDirectorClient.uploadTranslatable(translationsComDocument);
+            }
+
+            // Setting a Boolean value indicating that we successfully uploaded at least one document in the current submission.
+            context.Items[DocumentUploadedKey] = true;
+
+            translationId = translationsComDocument.name;
+
+            return false;
+        }
+
+        /// <summary>
+        /// TODOTR: document
+        /// </summary>
+        /// <param name="evnt">TODOTR: document event</param>
+        /// <param name="context">TODOTR: document context</param>
+        /// <param name="projectId">TODOTR: document projectId</param>
+        /// <returns>TODOTR: document returns</returns>
+
+        protected override bool ProcessStartProjectEvent(IStartProjectTaskEvent evnt, ITranslationJobContext context, out string projectId)
+        {
+            projectId = string.Empty;
+
+            var projectDirectorClient = new GLExchange(this.ProjectDirectorConfig);
+
+            var project = projectDirectorClient.getProject(this.TranslationsComProjectShortCode);
+
+            Submission submission = new Submission();
+
+            submission.name = string.Format("{0}{1}-UCF-{2}", this.SubmissionNamePrefix, evnt.ProjectName, DateTime.Now.ToString("yyyy-MM-dd-hh-mm"));
+            submission.project = project;
+            submission.dueDate = evnt.DueDate;
+
+            projectDirectorClient.initSubmission(submission);
+
+            context.Items[CurrentClientKey] = projectDirectorClient;
+            context.Items[StartProjectEventKey] = evnt;
+
+            return false;
+        }
+
+        /// <summary>
+        /// TODOTR: document
+        /// </summary>
+        /// <param name="evnt">TODOTR: document event</param>
+        /// <returns>TODOTR: document returns</returns>
+        protected override bool ProcessCompleteProjectEvent(ICompleteProjectTaskEvent evnt)
+        {
+            // Just acknowledge event
+            return true;
+        }
+
+        /// <summary>
+        /// TODOTR: document
+        /// </summary>
+        /// <param name="context">TODOTR: document context</param>
+        protected override void OnEndSendTranslationJob(ITranslationJobContext context)
+        {
+            GLExchange projectDirectorClient;
+            if (context.TryGetItem<GLExchange>(CurrentClientKey, out projectDirectorClient))
+            {
+                bool isAtLeastOneDocumentUploaded;
+                if (context.TryGetItem<bool>(DocumentUploadedKey, out isAtLeastOneDocumentUploaded) && isAtLeastOneDocumentUploaded)
+                {
+                    var submissionTicket = projectDirectorClient.getSubmissionTicket();
+                    IStartProjectTaskEvent startProjEvent;
+                    if (context.TryGetItem<IStartProjectTaskEvent>(StartProjectEventKey, out startProjEvent))
+                    {
+                        projectDirectorClient.startSubmission();
+                        startProjEvent.Acknowledge(submissionTicket);
+                    }
+                }
+
+                context.Items.Remove(CurrentClientKey);
+            }
+
+            base.OnEndSendTranslationJob(context);
+        }
+
+        #endregion
+
+        #region Sync methods
+
+        /// <inheritdoc />
+        protected override IEnumerable<object> GetRawMessages(ITranslationSyncContext context)
+        {
+            var client = this.GetClient(context);
+
+            var project = client.getProject(this.TranslationsComProjectShortCode);
+
+            return client.getCompletedTargets(project, 50);
+        }
+
+        /// <inheritdoc />
+        protected override IEnumerable<SyncEventMessage> ExtractSyncEventMessages(object rawMessage, ITranslationSyncContext context)
+        {
+            var client = this.GetClient(context);
+            var target = rawMessage as Target;
+
+            var ticket = target.ticket;
+
+            var xliffFile = this.GetXliffFile(client, ticket);
+            var message = new ReviewTranslationTaskEvent(xliffFile, rawMessage);
+
+            return new SyncEventMessage[] { message };
+        }
+
+        /// <inheritdoc />
+        protected override bool AcknowledgeMessage(object rawMessage, ITranslationSyncContext context)
+        {
+            var client = this.GetClient(context);
+            var result = client.sendDownloadConfirmation((rawMessage as Target).ticket);
+            return true;
+        }
+
+        /// <inheritdoc />
+        protected override void OnEndSyncTranslationJob(ITranslationSyncContext context)
+        {
+        }
+
+        #endregion
+
+        #region Inner class and structs
+
+        /// <summary>
+        /// Contains the string values of the keys used to configure the translations.com connector.
+        /// </summary>
+        public struct ConfigKeyConstants
+        {
+            /// <summary>
+            /// The configuration key for the Url setting.
+            /// </summary>
+            public const string UrlKey = "url";
+
+            /// <summary>
+            /// The configuration key for the user name setting.
+            /// </summary>
+            public const string UsernameKey = "username";
+
+            /// <summary>
+            /// The configuration key for the password setting.
+            /// </summary>
+            public const string PasswordKey = "password";
+
+            /// <summary>
+            /// The configuration key for the user agent setting.
+            /// </summary>
+            public const string UseragentKey = "userAgent";
+
+            /// <summary>
+            /// The configuration key for the project short code setting.
+            /// </summary>
+            public const string ProjectKey = "project";
+
+            /// <summary>
+            /// The configuration key for file format.
+            /// </summary>
+            public const string FileFormatKey = "fileFormatProfile";
+
+            /// <summary>
+            /// The configuration key for the submission name prefix.
+            /// </summary>
+            public const string PrefixKey = "submissionPrefix";
+        }
+        #endregion
+
+        #region Private methods
+
+        private XliffFile GetXliffFile(GLExchange client, string documentTicket)
+        {
+            using (var translatedDocumentStream = client.downloadTarget(documentTicket))
+            {
+                var serializer = new XmlSerializer(typeof(XliffRoot));
+
+                var xliff = (XliffRoot)serializer.Deserialize(translatedDocumentStream);
+                return xliff.Files[0];
+            }
+        }
+
+        private GLExchange GetClient(ITranslationContext context)
+        {
+            GLExchange client;
+            if (!context.TryGetItem<GLExchange>(CurrentClientKey, out client))
+            {
+                client = new GLExchange(this.ProjectDirectorConfig);
+                context.Items[CurrentClientKey] = client;
+            }
+
+            return client;
+        }
+
+        private void ProcessXliffFile(XliffFile file)
+        {
+            foreach (var unit in file.Body)
+            {
+                unit.Target = unit.Source;
+                unit.Source = null;
+            }
+        }
+
+        private ProjectDirectorConfig GetProjectDirectorConfig(NameValueCollection sitefinityConfig)
+        {
+            ProjectDirectorConfig projectDirectorConfig = new ProjectDirectorConfig();
+            string url = sitefinityConfig[ConfigKeyConstants.UrlKey];
+            if (string.IsNullOrEmpty(url))
+            {
+                this.ThrowConfigurationMissingError(ConfigKeyConstants.UrlKey);
+            }
+            else
+            {
+                projectDirectorConfig.url = url;
+            }
+
+            string username = sitefinityConfig[ConfigKeyConstants.UsernameKey];
+            if (string.IsNullOrEmpty(username))
+            {
+                this.ThrowConfigurationMissingError(ConfigKeyConstants.UsernameKey);
+            }
+            else
+            {
+                projectDirectorConfig.username = username;
+            }
+
+            string password = sitefinityConfig[ConfigKeyConstants.PasswordKey];
+            if (string.IsNullOrEmpty(password))
+            {
+                this.ThrowConfigurationMissingError(ConfigKeyConstants.PasswordKey);
+            }
+            else
+            {
+                projectDirectorConfig.password = password;
+            }
+
+            string userAgent = sitefinityConfig[ConfigKeyConstants.UseragentKey];
+            if (string.IsNullOrEmpty(userAgent))
+            {
+                userAgent = "Sitefinity";
+            }
+
+            projectDirectorConfig.userAgent = userAgent;
+            return projectDirectorConfig;
+        }
+
+        private void ThrowConfigurationMissingError(string missingConfigurationKey)
+        {
+            throw new Exception(string.Concat("Configuration option: '", missingConfigurationKey, "' for the Translations.Com connector is not set"));
+        }
+        #endregion
+
+        #region Fields & constants
+        private const string FileFormat = "XLIFF";
+        private const string CurrentClientKey = "projectDirectorClient";
+        private const string DocumentUploadedKey = "isTranslationComDocumentUploaded";
+        private const string StartProjectEventKey = "startProjectEvent";
+        #endregion
+    }
+}
